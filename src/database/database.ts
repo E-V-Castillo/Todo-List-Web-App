4d34ea831a3ded5f76eb24584e9f5e02798790f8
E-V-Castillo <purelyforcode@gmail.com>
2023-05-23 08:24:31 +0800

    init

---
+++ src/database/database.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Pool } from 'pg';
+
+@Injectable()
+export class Database {
+	private readonly pool: Pool;
+
+	constructor() {
+		this.pool = new Pool({
+			user: 'postgres',
+			password: 'Applemansanas1!',
+			host: 'localhost',
+			port: 5432,
+			database: 'todo_list_project_database',
+		});
+		this.pool
+			.connect()
+			.then(() => console.log('connected'))
+			.catch((err) => console.error('connection error', err.stack));
+	}
+
+	query(sql: string, params: any[] = []): Promise<any> {
+		return this.pool.query(sql, params);
+	}
+}
