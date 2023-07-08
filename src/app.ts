7574d5e0226c152d64366ab7c570a6a46565296e
E-V-Castillo <purelyforcode@gmail.com>
2023-07-08 10:04:37 +0800

    changed config on sessions

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,42 @@
-import session from 'express-session'
-import pgSession from 'connect-pg-simple'
-import express from 'express'
-import dotenv from 'dotenv'
-import cors, { CorsOptions } from 'cors'
+import session from 'express-session';
+import pgSession from 'connect-pg-simple';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors, { CorsOptions } from 'cors';
 
-import passport from 'passport'
-import { profileModel } from './models/profile'
-import { Strategy as LocalStrategy } from 'passport-local'
+import passport from 'passport';
+import { profileModel } from './models/profile';
+import { Strategy as LocalStrategy } from 'passport-local';
 
-import pool from './config/database'
+import pool from './config/database';
 
-import taskRouter from './routes/tasks/index'
-import categoryRouter from './routes/categories/index'
-import profileRouter from './routes/profiles/index'
-import indexRouter from './routes/index'
-import { isAuthenticated } from './middleware/isAuthenticated'
-import { handleRuntimeError } from './middleware/handleRuntimeError'
-import { handleServerError } from './middleware/handleServerError'
-import { PassportSchema } from './utils/passportZod'
+import taskRouter from './routes/tasks/index';
+import categoryRouter from './routes/categories/index';
+import profileRouter from './routes/profiles/index';
+import indexRouter from './routes/index';
+import { isAuthenticated } from './middleware/isAuthenticated';
+import { handleRuntimeError } from './middleware/handleRuntimeError';
+import { handleServerError } from './middleware/handleServerError';
+import { PassportSchema } from './utils/passportZod';
 
-dotenv.config()
+dotenv.config();
 
-export const app = express()
+export const app = express();
 
 const pgSessionStore = new (pgSession(session))({
     pool: pool,
-})
+});
 
 const corsOptions: CorsOptions = {
     allowedHeaders: ['Authorization', 'Content-Type'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     origin: 'http://localhost:5173',
     credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
     session({
@@ -44,12 +44,12 @@ app.use(
         saveUninitialized: false,
         resave: false,
         store: pgSessionStore,
-        cookie: { maxAge: 1000 * 60 * 60 },
+        cookie: { maxAge: 1000 * 60 * 60, httpOnly: true },
     })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // I hate passport
 passport.use(
@@ -60,56 +60,56 @@ passport.use(
                 const schemaResult = PassportSchema.safeParse({
                     email,
                     password,
-                })
+                });
                 if (!schemaResult.success) {
-                    throw schemaResult.error
+                    throw schemaResult.error;
                 }
-                const user = await profileModel.getProfileByEmail(email)
+                const user = await profileModel.getProfileByEmail(email);
                 if (!user) {
                     return done(null, false, {
                         message: 'Failed to find user with email',
-                    })
+                    });
                 }
                 if (user.password != password) {
-                    return done(null, false, { message: 'Invalid password' })
+                    return done(null, false, { message: 'Invalid password' });
                 }
-                console.log('Authentication successful for user')
-                console.log(user)
+                console.log('Authentication successful for user');
+                console.log(user);
 
-                return done(null, user)
+                return done(null, user);
             } catch (error) {
                 return done(error, false, {
                     message: 'Internal Server Message',
-                })
+                });
             }
         }
     )
-)
+);
 
 passport.serializeUser((user: any, done) => {
-    done(null, user.profile_id)
-})
+    done(null, user.profile_id);
+});
 
 passport.deserializeUser(async (id: string, done) => {
-    const userId = parseInt(id)
+    const userId = parseInt(id);
     try {
-        const result = await profileModel.getProfileById(userId)
-        done(null, result)
+        const result = await profileModel.getProfileById(userId);
+        done(null, result);
     } catch (error) {
-        done(error, false)
+        done(error, false);
     }
-})
+});
 
-app.use('/tasks', isAuthenticated, taskRouter)
-app.use('/categories', isAuthenticated, categoryRouter)
-app.use('/profiles', profileRouter)
-app.use('/', indexRouter)
-app.use(handleRuntimeError)
-app.use(handleServerError)
+app.use('/tasks', isAuthenticated, taskRouter);
+app.use('/categories', isAuthenticated, categoryRouter);
+app.use('/profiles', profileRouter);
+app.use('/', indexRouter);
+app.use(handleRuntimeError);
+app.use(handleServerError);
 
-const port = 3000
+const port = 3000;
 if (process.env.ENVIRONMENT !== 'testing') {
     app.listen(port, () => {
-        console.log('server started')
-    })
+        console.log('server started');
+    });
 }
